3e423ce800d40da7ee6d711f76bc402bc54be44f
AmashOnBlitz <myselfamash@gmail.com>
2026-02-27 17:33:21 +0530

    Chore : Added Basic d2d1 structure

--- Graphics.cpp
+++ Graphics.cpp
@@ -1,7 +1,49 @@
 #include "Graphics.hpp"
 
+#define CHECK_HR_OK if (hr != S_OK) return setInit(false);
+
+Graphics::Graphics(HWND hwnd) : mHwnd(hwnd);
+{
+}
+
+Graphics::~Graphics()
+{
+    if (mpfact) mpfact->Release();
+    if (mpRend) mpRend->Release();
+    mpfact = nullptr;
+    mpRend = nullptr;
+}
+
 Graphics &Graphics::Instance()
 {
     static Graphics g;
     return g;
+}
+
+bool Graphics::Init()
+{
+    HRESULT hr = D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, &mpfact);
+    CHECK_HR_OK; 
+
+    RECT rect;
+    GetClientRect(mHwnd, &rect);
+    hr = mpfact->CreateHwndRenderTarget(
+            D2D1::RenderTargetProperties(),
+            D2D1::HwndRenderTargetProperties(mHwnd, D2D1::SizeU(rect.right - rect.left, rect.bottom - rect.top)),
+            &mpRend
+        );
+    CHECK_HR_OK;
+
+    return setInit(true);
+}
+
+bool Graphics::setInit(bool b)
+{
+    isInit = b;
+    return b;
+}
+
+template <typename t>
+void ReleaseID2D1Object(t& object){
+    if (object) object->Release();
 }
--- Graphics.hpp
+++ Graphics.hpp
@@ -4,9 +4,22 @@
 #include <d2d1.h>
 
 class Graphics final {
-    public:
-    Graphics(){}
+public:
+    Graphics(HWND hwnd);
+    ~Graphics();
     Graphics& Instance();
+    bool Init();
+
+private: // funtions
+    bool setInit(bool b);
+    template <typename t>
+    void ReleaseID2D1Object(t& object);
+private:
+    bool isInit = false;
+    HWND mHwnd;
+    ID2D1Factory* mpfact;
+    ID2D1RenderTarget* mpRend;
 };
 
 #endif //!GRAPHICS_RCINWIN32_HEADER
+
